9ae99651f2b9e4fe36eef306e3bee247e5c81d48
jpalencia77 <50339058+jpalencia77@users.noreply.github.com>
2019-06-12 19:38:34 -0500

    Delete Car.js

--- 3p/ES6mod/Car.js
+++
@@ -1,8 +0,0 @@
-class Car {
-    constructor(name){
-        this.name = name;
-    }
-    drive(){    }
-    start(){return this.name + "engine started..." + "\n"; }
-    toString(){return "hi, my name is " + this.name;}
-}
