51aa8211d402fcf2ded2b37c08821b6a3f85e295
jpalencia77 <50339058+jpalencia77@users.noreply.github.com>
2019-06-12 19:35:20 -0500

    Delete tempConvert.js

--- 3p/html/events/tempConvert.js
+++
@@ -1,8 +0,0 @@
-//
-// this may be a class, object, module, or function
-//
-function convert() {
-    let t = document.getElementById("temp").value
-    let o = (t-32)/1.8
-    document.getElementById("out").innerHTML = o.toFixed(2) + " degrees F"
-}
