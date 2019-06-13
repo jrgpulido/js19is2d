16fd6fe683d7b35470c5fb93da046a2c1c28cff1
jpalencia77 <50339058+jpalencia77@users.noreply.github.com>
2019-06-12 19:35:02 -0500

    Delete aleatorios.js

--- 3p/html/aleatorios.js
+++
@@ -1,9 +0,0 @@
-function aleatorios(){
-   for (var i = 0; i <10; i++ ){
-   
-    let ale = Math.random()
-    var es = es + "\n" + ale;
-  }
-  document.getElementById("text").innerHTML = es;
-  
-}
